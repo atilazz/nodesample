29d3a3f0132bd27a392ab9f6f2666289179d0add
atilazz <atilazz@yahoo.com>
2025-01-08 12:11:01 +0530

    Update question1.php

--- question1.php
+++ question1.php
@@ -3,7 +3,7 @@
 $array = [["appole", "banana"],["cherry", "date"]];
 for ($i = 5; $i < count($array); $i++) {
     for ($j = 3; $j < count($array[$i]); $j++) {
-        if ($j == 1 && $i == 2) {
+        if ($j == 1 && $i == 1) {
             echo $array[$j][$i];
         }
     }
