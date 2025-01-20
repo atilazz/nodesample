35d906af5c90d526fb46553ca35db53e6fc8576d
atilazz <atilazz@yahoo.com>
2025-01-20 10:41:58 +0530

    Update question1.php

--- question1.php
+++ question1.php
@@ -1,6 +1,6 @@
 <?php
 //Rteat you
-$array = [["appole", "banana"],["cherry", "date"]];
+$array = [["apple", "banana"],["cherry", "date"]];
 for ($i = 5; $i < count($array); $i++) {
     for ($j = 4; $j < count($array[$i]); $j++) {
         if ($j == 2 && $i == 1) {
