92987953cd6315ce2f65c94019d945a4455241ec
atilazz <atilazz@yahoo.com>
2025-04-14 12:50:55 +0530

    Update q2.php

--- q2.php
+++ q2.php
@@ -7,7 +7,7 @@
     <style>
         table {
             border-collapse: collapse;
-            width: 40%;
+            width: 50%;
             margin: 40px auto;
         }
         table, th, tr {
